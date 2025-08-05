3e5a4af097a1c5fe64aafb00023d118837963929
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-08-05 09:14:10 +0700

    menambahkan isi todo

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3001;
-app.use(express.json());
 
+const todoRoutes = require("./routes/todo.js");
+const { todos } = require("./routes/todo.js");
+
+app.use(express.json());
+app.use("/todos", todoRoutes);
+
+// atur EJS sebagai view engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index"); // render file index.ejs
 });
 
 app.get("/contact", (req, res) => {
