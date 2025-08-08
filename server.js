af57f98c1735cfb377f9ed9bd8cd4d42082a2bd1
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-08-08 09:16:21 +0700

    membuat port env

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3001;
+//const port = 3001;
+require("dotenv").config();
+const port = process.env.PORT;
 
 const todoRoutes = require("./routes/todo.js");
 const { todos } = require("./routes/todo.js");
